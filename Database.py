1b01005d27a0f5a2c928900dda3281a18fcb832a
CS <43545314+Claire-Sophie@users.noreply.github.com>
2020-10-05 21:19:54 +0200

    Add files via upload

--- Database.py
+++ Database.py
@@ -1,25 +1,34 @@
-
-
+import copy
 class Database(object):
 
 
 	def __init__(self ,  node_parent ):
 
 		node = Nodes((node_parent , None))
+		self.tree = {node_parent : None}
 		self.root = node
-		self.dico = {}
+	
 
 	def add_nodes(self , list_tuple):
+		before_update = copy.deepcopy(self.tree)
 
 		for i in set(list_tuple):
-			Nodes.add_child(self.root , i[0])
+			Nodes.add_child(self.root, i[0])
+			self.tree[i[0]] = i[1]
+
+		for i , j  in self.tree.items():
+			val2= before_update.get(i)
+			if val2 is not 'core' and val2 is not None:
+				self.gran = val2
+				print(self.gran)
+				self.state = 'Coverage stage'
 
 		self.graph = list_tuple
 
 		list_child = []
 
-		for i , j   in self.graph :
-			list_child.append(self.recursive_dfs(self.graph , i ))
+		for i , j  in self.graph :
+			list_child.append(self.recursive_dfs(self.graph , i))
 
 		self.enfant = list_child[0]
 
@@ -27,12 +36,12 @@ class Database(object):
 	def recursive_dfs(self, tree, node , find =[] , neighbour = None):
 
 		if node not in find:
+
 			find.append(node)
 			if node not in tree:
 				return find
 			for neighbour in tree[node]:
 				find = self.recursive_dfs(tree, neighbour, find)
-
 		return find , neighbour
 
 
@@ -49,27 +58,26 @@ class Database(object):
 		for i , j   in self.dico.items():
 			key = i
 			self.status[key] = 'Valid'
-			print('child' , self.enfant)
 
 			for e in j :
-				print('e' , e)
+
 				if e not in self.enfant :
-					stat = 'Invalid'
-				if e in self.enfant:
-					stat ='Valid'
+					self.stat = 'Invalid'
+				if e in self.enfant :
+					self.stat ='Valid'
 
-				if e in self.enfant and e  in self.enfant:
-						stat ='Granularity'
+				if e in self.enfant and len(self.graph)>0:
+					self.stat ='Granularity'
 
-			self.status.update({key: stat})
+				if self.gran :
+					"IN PROGRESS"
+
+
+
+
+			self.status.update({key: self.stat})
 		print(self.status)
 
-	def get_parent(self, node):
-
-		for i in range(len(self.graph)):
-			if self.graph[i][0] == node:
-				par = self.graph[i][1]
-		return par
 
 class Nodes:
 
@@ -87,6 +95,10 @@ class Nodes:
 			self.nodesparent == self.nodesnew
 
 		self.list.append(self.nodes_child)
+
+
+
+
 
 
 
